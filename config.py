fd1c16e8eeda97c2e8e196dbc8d96e418ad97d50
rallenlove <ross@threesparrows.net>
2022-02-08 11:12:38 -0600

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-consumer_key = 'Kyumksnvc4t56l9KPVh4ZLuqg'
-consumer_secret = 'WWVdovP96hAePQnHnpHRaxJkVja74YBKEqvRcsK8dq2HIPMMrm'
-access_token = '363744127-5yYepikJd1epjJtJ6U3ATJmV3spjLD8PJOGrHONR'
-access_token_secret = 'VyEURVcQ3uj4mrRRT3m1XlX9iBVsuXEhPXAdow2Y8UNzu'
